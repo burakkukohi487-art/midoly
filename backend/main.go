c2ba34ba693f7bf718b8ca57c51181241a8853ba
TeramuraKohei <burakkukohi487@gmail.com>
2026-06-30 11:07:01 +0900

    feat: goの初期化

---
+++ backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
